438faee2ba59e750ec557e1caa921f03184e3e26
BhavayMittal27 <bhavaymittal35450@gmail.com>
2026-02-17 09:04:45 +0530

    UC2 added

---
+++ OOPS.java
@@ -0,0 +1,13 @@
+ class OOPS {
+    public static void main(String[] args) {
+
+        System.out.println("*****  *****  ****   *****");
+        System.out.println("*   *  *   *  *   *  *");
+        System.out.println("*   *  *   *  *   *  *");
+        System.out.println("*   *  *   *  ****   *****");
+        System.out.println("*   *  *   *  *          *");
+        System.out.println("*   *  *   *  *          *");
+        System.out.println("*****  *****  *      *****");
+
+    }
+}
