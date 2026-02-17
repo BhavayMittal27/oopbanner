a17ed6e8c0a9315051bd83cc2c8e98cb409cfed8
BhavayMittal27 <bhavaymittal35450@gmail.com>
2026-02-17 08:43:19 +0530

    UC2 ADDED

--- oop_main.java
+++
@@ -1,5 +0,0 @@
-class oop_main{
-	public static void main(String args[]){
-		System.out.println("OOP Banner app");
-	}
-}
--- step.java
+++
@@ -1,5 +0,0 @@
-class step{
-	public static void main(String args[]){
-		System.out.println("OOP Banner app");
-	}
-}
